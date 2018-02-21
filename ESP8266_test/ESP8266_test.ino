680a92b5777fb9b4950c16482981e6349295d0a8
theqb <31521302+theqb@users.noreply.github.com>
2018-02-21 16:02:41 -0600

    Updated the code to work with ESP01 Modules

--- ESP8266_test/ESP8266_test.ino
+++ ESP8266_test/ESP8266_test.ino
@@ -16,113 +16,104 @@
  1) Wire Module:
  Wire according to this picture: https://www.forward.com.au/pfod/ESP8266/GPIOpins/ESP8266_01.jpg
  - Connect VCC and CH_PD to 3.3V on the arduino
- - Connect RX to RX1 (Digital 0 on the arduino) !!! but may need voltage shifting (see below) !!!
- - Connect TX to TX1 (Digital 1 on the arduino)
+ - Connect RX to TX on the programmer
+ - Connect TX to RX on the programmer
  - Connect GND to GND
- The RX pin is 3.3V logic while the Arudino is 5V, so you may need to make
- a voltage divider to drop the voltage. To do so, use the formula
- VOUT = VIN(R2/(R1+R2)) so choosing like R1 = 1K and R2 = 2K will work
- You can use resistors from your EE302/319K kits or see the gear checkout desk in EER
- to get required supplies. You neeed to have 5V turn into 3 - 3.3V, so calculate accordingly!
- Diagram of voltage divider: https://cdn.sparkfun.com/r/300-300/assets/c/8/8/7/7/514208cbce395f1c12000000.png
+ - Connect GPIO2 to 3.3V with a resistor between 1K and 50K ohms 
+ (If you can't find a resistor, you can use some from your EE302/319K kit or ask the equipment 
+ checkout desk in EER for one)
 
  2) Install Module Drivers and Select the Board:
- Go to File -> Preferences and paste this link into aditional baord manager url's:
+ Go to File -> Preferences and paste this link into aditional board manager url's:
  http://arduino.esp8266.com/stable/package_esp8266com_index.json
  Then, go to Tools -> Boards: -> Boards Manager and search "ESP8266" and install the first item
  Go once more to Tools -> Boards but scroll down and select "Generic ESP8266 module"
 
- 3) Install Library:
- Go to Sketch -> Include Library -> Manage Libraries
- and search 'WiFiEsp' in the popup window
- and click install on the first result (should be by bportaluri)
- Go to your Arduino folder on your computer (usually in Documents for windows)
- and navigate to Arduino -> libraries -> WiFiEsp -> src -> utility -> EspDrv.cpp
- and change '#include <avr/pgmspace.h>' to '#include <pgmspace.h>' in the file
-
- 4) Change the network name and password on line 59 and 60 for your hardware
+ 4) Change the network name and password on line 50 and 51 for your hardware
  
- 5) Program your Code onto the arduino
+ 5) To program, unplug your board and connect the GPIO2 to GND with another wire (while it is 
+ still pulled up by resistor) then plug the programmer back in, turing on the board. Next, unplug the 
+ jumper to ground you just added, select the programmer's COM port, and click the upload button!
 
  6) Open a serial monitor by going to
- Tools -> Serial Monitor and set to 115200 baud once open
+ Tools -> Serial Monitor and set to 115200 baud once open to see output
 
- 7) Run! 
+ 7) Test and make sure it is working! If you see '......' on the serial
+ monitor, you are not connecting to your wifi network correctly.
  
  ******************************************************/
  
 // Library defs
-#include "WiFiEsp.h"
-#include "WiFiEspUdp.h"
+#include <ESP8266WiFi.h>
+#include <WiFiUdp.h>
 
-// !!!!!!!!!!!!!!!Update for network being used!!!!!!!!!!!!!!!!!!!!!!!!!!!
-char ssid[] = "yourNetwork";     // your network SSID (name) within quotes
-char pass[] = "secretPassword";  // your network password within quotes
+char ssid[] = "**********";  // your network SSID (name)
+char pass[] = "**********";  // your network password
 
-// Emulate Serial2 on pins 6/7 if not present
-#ifndef HAVE_HWSerial2
-#include "SoftwareSerial.h"
-SoftwareSerial Serial2(6, 7); // RX, TX
-#endif
 
-int status = WL_IDLE_STATUS;     // the Wifi radio's status
+unsigned int localPort = 2390;      // local port to listen for UDP packets
 
-char timeServer[] = "time.nist.gov";  // NTP server
-unsigned int localPort = 2390;        // local port to listen for UDP packets
+/* Don't hardwire the IP address or we won't get the benefits of the pool.
+ *  Lookup the IP address for the host name instead */
+//IPAddress timeServer(129, 6, 15, 28); // time.nist.gov NTP server
+IPAddress timeServerIP; // time.nist.gov NTP server address
+const char* ntpServerName = "time.nist.gov";
 
-const int NTP_PACKET_SIZE = 48;  // NTP timestamp is in the first 48 bytes of the message
-const int UDP_TIMEOUT = 2000;    // timeout in miliseconds to wait for an UDP packet to arrive
+const int NTP_PACKET_SIZE = 48; // NTP time stamp is in the first 48 bytes of the message
 
-byte packetBuffer[NTP_PACKET_SIZE]; // buffer to hold incoming and outgoing packets
+byte packetBuffer[ NTP_PACKET_SIZE]; //buffer to hold incoming and outgoing packets
 
 // A UDP instance to let us send and receive packets over UDP
-WiFiEspUDP Udp;
+WiFiUDP udp;
 
 void setup()
 {
-  // initialize serial for debugging
   Serial.begin(115200);
-  // initialize serial for ESP module
-  Serial2.begin(9600);
-  // initialize ESP module
-  WiFi.init(&Serial2);
+  Serial.println();
+  Serial.println();
 
-  // check for the presence of the shield
-  if (WiFi.status() == WL_NO_SHIELD) {
-    Serial.println("WiFi shield not present");
-    // don't continue
-    while (true);
-  }
-
-  // attempt to connect to WiFi network
-  while ( status != WL_CONNECTED) {
-    Serial.print("Attempting to connect to WPA SSID: ");
-    Serial.println(ssid);
-    // Connect to WPA/WPA2 network
-    status = WiFi.begin(ssid, pass);
-  }
-
-  // you're connected now, so print out the data
-  Serial.println("You're connected to the network");
+  // We start by connecting to a WiFi network
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, pass);
   
-  Udp.begin(localPort);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  Serial.println("Starting UDP");
+  udp.begin(localPort);
+  Serial.print("Local port: ");
+  Serial.println(udp.localPort());
 }
 
 void loop()
 {
-  sendNTPpacket(timeServer); // send an NTP packet to a time server
+  //get a random server from the pool
+  WiFi.hostByName(ntpServerName, timeServerIP); 
+
+  sendNTPpacket(timeServerIP); // send an NTP packet to a time server
+  // wait to see if a reply is available
+  delay(1000);
   
-  // wait for a reply for UDP_TIMEOUT miliseconds
-  unsigned long startMs = millis();
-  while (!Udp.available() && (millis() - startMs) < UDP_TIMEOUT) {}
+  int cb = udp.parsePacket();
+  if (!cb) {
+    Serial.println("no packet yet");
+  }
+  else {
+    Serial.print("packet received, length=");
+    Serial.println(cb);
+    // We've received a packet, read the data from it
+    udp.read(packetBuffer, NTP_PACKET_SIZE); // read the packet into the buffer
 
-  Serial.println(Udp.parsePacket());
-  if (Udp.parsePacket()) {
-    Serial.println("packet received");
-    // We've received a packet, read the data from it into the buffer
-    Udp.read(packetBuffer, NTP_PACKET_SIZE);
-
-    // the timestamp starts at byte 40 of the received packet and is four bytes,
+    //the timestamp starts at byte 40 of the received packet and is four bytes,
     // or two words, long. First, esxtract the two words:
 
     unsigned long highWord = word(packetBuffer[40], packetBuffer[41]);
@@ -130,7 +121,7 @@ void loop()
     // combine the four bytes (two words) into a long integer
     // this is NTP time (seconds since Jan 1 1900):
     unsigned long secsSince1900 = highWord << 16 | lowWord;
-    Serial.print("Seconds since Jan 1 1900 = ");
+    Serial.print("Seconds since Jan 1 1900 = " );
     Serial.println(secsSince1900);
 
     // now convert NTP time into everyday time:
@@ -147,13 +138,13 @@ void loop()
     Serial.print("The UTC time is ");       // UTC is the time at Greenwich Meridian (GMT)
     Serial.print((epoch  % 86400L) / 3600); // print the hour (86400 equals secs per day)
     Serial.print(':');
-    if (((epoch % 3600) / 60) < 10) {
+    if ( ((epoch % 3600) / 60) < 10 ) {
       // In the first 10 minutes of each hour, we'll want a leading '0'
       Serial.print('0');
     }
     Serial.print((epoch  % 3600) / 60); // print the minute (3600 equals secs per minute)
     Serial.print(':');
-    if ((epoch % 60) < 10) {
+    if ( (epoch % 60) < 10 ) {
       // In the first 10 seconds of each minute, we'll want a leading '0'
       Serial.print('0');
     }
@@ -164,13 +155,13 @@ void loop()
 }
 
 // send an NTP request to the time server at the given address
-void sendNTPpacket(char *ntpSrv)
+unsigned long sendNTPpacket(IPAddress& address)
 {
+  Serial.println("sending NTP packet...");
   // set all bytes in the buffer to 0
   memset(packetBuffer, 0, NTP_PACKET_SIZE);
   // Initialize values needed to form NTP request
   // (see URL above for details on the packets)
-
   packetBuffer[0] = 0b11100011;   // LI, Version, Mode
   packetBuffer[1] = 0;     // Stratum, or type of clock
   packetBuffer[2] = 6;     // Polling Interval
@@ -183,10 +174,7 @@ void sendNTPpacket(char *ntpSrv)
 
   // all NTP fields have been given values, now
   // you can send a packet requesting a timestamp:
-  Udp.beginPacket(ntpSrv, 123); //NTP requests are to port 123
-
-  Udp.write(packetBuffer, NTP_PACKET_SIZE);
-
-  Udp.endPacket();
+  udp.beginPacket(address, 123); //NTP requests are to port 123
+  udp.write(packetBuffer, NTP_PACKET_SIZE);
+  udp.endPacket();
 }
-
