b7cb5227ae4aa6214c46667438931828a27bbb90
Andrew Lynch <lynch.andrew@utexas.edu>
2018-02-27 17:07:59 -0600

    blynk with macro

---
+++ Blynk_test/Blynk_test.ino
@@ -0,0 +1,88 @@
+/*************************************************************
+  Download latest Blynk library here:
+    https://github.com/blynkkk/blynk-library/releases/latest
+
+  Blynk is a platform with iOS and Android apps to control
+  Arduino, Raspberry Pi and the likes over the Internet.
+  You can easily build graphic interfaces for all your
+  projects by simply dragging and dropping widgets.
+
+    Downloads, docs, tutorials: http://www.blynk.cc
+    Sketch generator:           http://examples.blynk.cc
+    Blynk community:            http://community.blynk.cc
+    Follow us:                  http://www.fb.com/blynkapp
+                                http://twitter.com/blynk_app
+
+  Blynk library is licensed under MIT license
+  This example code is in public domain.
+
+ *************************************************************
+
+  You’ll need:
+   - Blynk App (download from AppStore or Google Play)
+   - ESP8266 board
+   - Decide how to connect to Blynk
+     (USB, Ethernet, Wi-Fi, Bluetooth, ...)
+
+  There is a bunch of great example sketches included to show you how to get
+  started. Think of them as LEGO bricks  and combine them as you wish.
+  For example, take the Ethernet Shield sketch and combine it with the
+  Servo example, or choose a USB sketch and add a code from SendData
+  example.
+ *************************************************************/
+
+/* Comment this out to disable prints and save space */
+#define BLYNK_PRINT Serial
+
+
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
+
+// You should get Auth Token in the Blynk App.
+// Go to the Project Settings (nut icon).
+char auth[] = "51fd8e1456fc4a8da0a864f15748ed99";
+
+// Your WiFi credentials.
+// Set password to "" for open networks.
+char ssid[] = "fydetest";
+char pass[] = "fydetest";
+
+#define VirtualPin_Handler(pin_num) \
+BLYNK_WRITE(pin_num)                \
+{                                   \
+  Serial.print(#pin_num "~");       \
+  Serial.print(param.asInt());      \
+  Serial.print("~");                \
+  Serial.print(param.asStr());      \
+  Serial.print("~");                \
+  Serial.println(param.asDouble()); \
+}                         
+
+VirtualPin_Handler(V1)
+
+void recvCred(void)
+{
+  
+}
+
+void setup()
+{
+  // Debug console
+  Serial.begin(9600);
+
+  recvCred();
+
+  Blynk.begin(auth, ssid, pass);
+  // You can also specify server:
+  //Blynk.begin(auth, ssid, pass, "blynk-cloud.com", 8442);
+  //Blynk.begin(auth, ssid, pass, IPAddress(192,168,1,100), 8442);
+}
+
+void loop()
+{
+  Blynk.run();
+  // You can inject your own code or combine it with other sketches.
+  // Check other examples on how to communicate with Blynk. Remember
+  // to avoid delay() function!
+}
+
